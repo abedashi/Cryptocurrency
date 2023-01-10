db047ed28617dd7d201144d4f3b9b2ab53ab9cb8
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-01-10 06:30:21 +0200

    sidebar and navbar depends on size screen

---
+++ client/src/components/UI/Navbar.jsx
@@ -0,0 +1,67 @@
+const Navbar = () => {
+  return (
+    <nav className="max-xl:block hidden bg-white border border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+      <div className="container flex flex-wrap items-center justify-between mx-auto">
+        <a href="https://flowbite.com/" class="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            CRYPTO<spna className="text-[#D0ED6C]">X</spna>
+          </span>
+        </a>
+        <div className="flex items-center md:order-2">
+          <button
+            type="button"
+            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-[#D0ED6C] dark:focus:ring-[#D0ED6C]"
+            id="user-menu-button"
+            aria-expanded="false"
+            data-dropdown-toggle="user-dropdown"
+            data-dropdown-placement="bottom"
+          >
+            <span className="sr-only">Open user menu</span>
+            <img
+              class="w-8 h-8 rounded-full"
+              src="/docs/images/people/profile-picture-3.jpg"
+              alt="user"
+            />
+          </button>
+          <div
+            className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+            id="user-dropdown"
+          >
+            <div className="px-4 py-3">
+              <span className="block text-sm text-gray-900 dark:text-white">
+                Bonnie Green
+              </span>
+              <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+                name@flowbite.com
+              </span>
+            </div>
+            <ul className="py-1" aria-labelledby="user-menu-button">
+              <li>
+                <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                  Dashboard
+                </span>
+              </li>
+              <li>
+                <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                  Settings
+                </span>
+              </li>
+              <li>
+                <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                  Earnings
+                </span>
+              </li>
+              <li>
+                <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#D0ED6C] dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                  Sign out
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
