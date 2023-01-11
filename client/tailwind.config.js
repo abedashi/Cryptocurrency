b79c524f1191e4906998df30d56d5a17556e08a6
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-01-11 21:16:11 +0200

    extends new primary color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5E7FA6",
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
