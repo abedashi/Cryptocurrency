f627593342593fc85d44510d379f59ea23f66524
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-01-14 04:51:46 +0200

    add utils api's for git single coin and all coins

---
+++ client/src/utils/APIs.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.coingecko.com/api/v3";
+
+export const getCoins = async () => {
+  const { data } = await axios.get(
+    BASE_URL +
+    "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+  );
+
+  try {
+    let loadedData = [];
+    for (const coin of data) {
+      loadedData.push({
+        id: coin.id,
+        name: coin.name,
+        rank: coin.market_cap_rank,
+        image: coin.image,
+        symbol: coin.symbol,
+        price: coin.current_price,
+        volume: coin.total_volume,
+        perDay: coin.price_change_24h,
+        market_cap: coin.market_cap,
+      });
+    }
+
+    return loadedData;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export const getCoin = async (id) => {
+  const { data } = await axios.get(
+    BASE_URL +
+    `/coins/${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`
+  );
+
+  try {
+    let loadedData = {
+      id: data.id,
+      name: data.name,
+      rank: data.market_cap_rank,
+      symbol: data.symbol,
+      image: data.image.small,
+      price: data.market_data.current_price.usd,
+    };
+
+    return loadedData;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
