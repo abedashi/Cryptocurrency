5c5f30cb88a80149f336333e963c7e397f53a4dc
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-01-21 06:09:44 +0200

    production setup

--- server/server.js
+++ server/server.js
@@ -18,12 +18,12 @@ app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/trades', require('./routes/tradeRoutes'));
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')));
-} else {
-    app.get('/', (req, res) => res.send('Please set to production'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/build')));
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')));
+// } else {
+//     app.get('/', (req, res) => res.send('Please set to production'));
+// }
 
 app.use(errorHandler);
 
